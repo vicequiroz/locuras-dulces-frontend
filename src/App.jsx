c1bf79acd7b8a382d50f7fe02b669bde491101a0
Vicente Quiroz <vice.quiroz@duocuc.cl>
2025-10-11 15:48:34 -0300

    El  home queda ok con todos sus componentes, pero no se visualiza  como en HTML. Se debe modificar

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,20 @@ import './App.css';
 
 // 🧩 Páginas
 import { Home } from './pages/Home/Home';
-import { Contacto } from './pages/Contacto/Contacto';
-import { Inventario } from './pages/Inventario/Inventario';
-import { Blogs } from './pages/Blogs/Blogs';
-import { Nosotros } from './pages/Nosotros/Nosotros';
-import { Carrito } from './pages/Carrito/Carrito';
-import { Login } from './pages/Login/Login';
-import { Registro } from './pages/Registro/Registro';
-import { Dashboard } from './pages/Dashboard/Dashboard';
-import { DetalleProducto } from './pages/DetalleProducto/DetalleProducto';
+//import { Contacto } from './pages/Contacto/Contacto';
+//import { Inventario } from './pages/Inventario/Inventario';
+//import { Blogs } from './pages/Blogs/Blogs';
+//import { Nosotros } from './pages/Nosotros/Nosotros';
+//import { Carrito } from './pages/Carrito/Carrito';
+//import { Login } from './pages/Login/Login';
+//import { Registro } from './pages/Registro/Registro';
+//import { Dashboard } from './pages/Dashboard/Dashboard';
+//import { DetalleProducto } from './pages/DetalleProducto/DetalleProducto';
 
 // 🧩 Componentes de gestión
-import { CrearProducto } from './componentes/CrearProd/CrearProducto';
-import { EditarProducto } from './componentes/EditarProd/EditarProducto';
-import { GestionUsuarios } from './componentes/GestionUsuarios/GestionUsuarios';
+// import { CrearProducto } from './componentes/CrearProd/CrearProducto';
+// import { EditarProducto } from './componentes/EditarProd/EditarProducto';
+// import { GestionUsuarios } from './componentes/GestionUsuarios/GestionUsuarios';
 
 function App() {
   return (
@@ -24,20 +24,20 @@ function App() {
       <Routes>
         {/* 🏠 Páginas principales */}
         <Route path="/" element={<Home />} />
-        <Route path="/contacto" element={<Contacto />} />
+        {/* <Route path="/contacto" element={<Contacto />} />
         <Route path="/inventario" element={<Inventario />} />
         <Route path="/blogs" element={<Blogs />} />
         <Route path="/nosotros" element={<Nosotros />} />
         <Route path="/carrito" element={<Carrito />} />
         <Route path="/login" element={<Login />} />
         <Route path="/registro" element={<Registro />} />
-        <Route path="/detalleproducto/:id" element={<DetalleProducto />} />
+        <Route path="/detalleproducto/:id" element={<DetalleProducto />} /> */}
 
         {/* 🛠️ Módulos administrativos */}
-        <Route path="/dashboard" element={<Dashboard />} />
+        {/* <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/crear-producto" element={<CrearProducto />} />
         <Route path="/editar-producto/:id" element={<EditarProducto />} />
-        <Route path="/usuarios" element={<GestionUsuarios />} />
+        <Route path="/usuarios" element={<GestionUsuarios />} /> */}
       </Routes>
     </Router>
   );
